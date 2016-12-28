f63374405137c1b05778a7e836ac46b528d503ad
Sergio Ortega <sjfox94@gmail.com>
2016-12-28 00:16:16 -0800

    Setup Repo with Initial Commit
    No significant progress atm

---
+++ app/src/main/java/com/forcerift/sjfox/project_bjet/MainActivity.java
@@ -0,0 +1,13 @@
+package com.forcerift.sjfox.project_bjet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
